55dd962a7afb05346cf308d176a722f39b53c952
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-07-30 16:11:08 +0300

    Add white-black tests for pawns

--- tests/blackPawn.cpp
+++ tests/blackPawn.cpp
@@ -7,14 +7,16 @@ int main(int argc, char** argv) {
 	using namespace std;
 
 	Chessboard cb;
-	Piece* pawn1 = new Pawn({'c', 2}, Piece::Color::White, cb);
-	Piece* pawn2 = new Pawn({'d', 3}, Piece::Color::Black, cb);
+	Piece* pawn1 = new Pawn({'d', 7}, Piece::Color::Black, cb);
+	Piece* pawn2 = new Pawn({'c', 6}, Piece::Color::White, cb);
+	Piece* pawn3 = new Pawn({'e', 6}, Piece::Color::White, cb);
 
-	Piece::MoveMapT movemap = pawn1->moveMap();
+	Piece::MoveMapT movemap = cb.possibleMoves({'d', 7});
 	Piece::MoveMapT targetmap = {
-		{pawn1, {'d', 3}, pawn2},
-		{pawn1, {'c', 4}}, 
-		{pawn1, {'c', 3}},
+		{pawn1, {'c', 6}, pawn2},
+		{pawn1, {'e', 6}, pawn3},
+		{pawn1, {'d', 5}},
+		{pawn1, {'d', 6}},
 	};;
 
 	cout << "expected:" << endl;
---
+++ tests/whitePawn.cpp
@@ -0,0 +1,30 @@
+#include "dchess.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+	using namespace dchess;
+	using namespace std;
+
+	Chessboard cb;
+	Piece* pawn1 = new Pawn({'c', 2}, Piece::Color::White, cb);
+	Piece* pawn2 = new Pawn({'d', 3}, Piece::Color::Black, cb);
+
+	Piece::MoveMapT movemap = cb.possibleMoves({'c', 2});
+	Piece::MoveMapT targetmap = {
+		{pawn1, {'d', 3}, pawn2},
+		{pawn1, {'c', 4}}, 
+		{pawn1, {'c', 3}},
+	};;
+
+	cout << "expected:" << endl;
+	for (auto x : targetmap)
+		cout << x << ' ';
+	cout << endl << "got:" << endl;
+	for (auto x : movemap)
+		cout << x << ' ';
+	
+	cout << endl << cb;
+
+	return !(movemap == targetmap);
+}
