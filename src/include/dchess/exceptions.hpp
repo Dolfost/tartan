e192c138e129e6ce2f6344b9179ce29daf248cad
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-08-01 12:01:00 +0300

    Format code indentation

--- src/include/dchess/exceptions.hpp
+++ src/include/dchess/exceptions.hpp
@@ -5,35 +5,35 @@
 #include<string>
 
 namespace dchess {
-	using string = std::string;
-	using logic_error = std::logic_error;
+using string = std::string;
+using logic_error = std::logic_error;
 
-	class illegal_move : logic_error {
-		public:
-			using logic_error::what;
-			illegal_move(const string& what_arg) :
-				logic_error(what_arg) {};
-			illegal_move(const char* what_arg) :
-				logic_error(what_arg) {};
-	};
+class illegal_move : logic_error {
+public:
+	using logic_error::what;
+	illegal_move(const string& what_arg) :
+		logic_error(what_arg) {};
+	illegal_move(const char* what_arg) :
+		logic_error(what_arg) {};
+};
 
-	class illegal_turn : logic_error {
-		public:
-			using logic_error::what;
-			illegal_turn(const string& what_arg) :
-				logic_error(what_arg) {};
-			illegal_turn(const char* what_arg) :
-				logic_error(what_arg) {};
-	};
+class illegal_turn : logic_error {
+public:
+	using logic_error::what;
+	illegal_turn(const string& what_arg) :
+		logic_error(what_arg) {};
+	illegal_turn(const char* what_arg) :
+		logic_error(what_arg) {};
+};
 
-	class illegal_place : logic_error {
-		public:
-			using logic_error::what;
-			illegal_place(const string& what_arg) :
-				logic_error(what_arg) {};
-			illegal_place(const char* what_arg) :
-				logic_error(what_arg) {};
-	};
+class illegal_place : logic_error {
+public:
+	using logic_error::what;
+	illegal_place(const string& what_arg) :
+		logic_error(what_arg) {};
+	illegal_place(const char* what_arg) :
+		logic_error(what_arg) {};
+};
 
 }
 
