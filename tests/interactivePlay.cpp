c616700467d7ac1f5d002e799536af896b6f90c5
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-08-05 10:18:23 +0300

    Add new pieces

--- tests/interactivePlay.cpp
+++ tests/interactivePlay.cpp
@@ -18,11 +18,17 @@ int main(int argc, char** argv) {
 	cb.setPieceGetter(getPiece);
 	std::vector<Pawn*> black(8), white(8);
 	for (int i = 0; i < 8; i++) {
-		white[i] = new Pawn({i+1, 2}, Piece::Color::White);
-		black[i] = new Pawn({i+1, 7}, Piece::Color::Black);
-		cb.insertPiece(white[i]);
-		cb.insertPiece(black[i]);
+		white[i] = new Pawn({i+1, 2}, Piece::Color::White, &cb);
+		black[i] = new Pawn({i+1, 7}, Piece::Color::Black, &cb);
 	}
+	new Bishop("c1", Piece::Color::White, &cb);
+	new Bishop("f1", Piece::Color::White, &cb);
+	new Bishop("c8", Piece::Color::Black, &cb);
+	new Bishop("f8", Piece::Color::Black, &cb);
+	new Knight("b1", Piece::Color::White, &cb);
+	new Knight("g1", Piece::Color::White, &cb);
+	new Knight("b8", Piece::Color::Black, &cb);
+	new Knight("g8", Piece::Color::Black, &cb);
 
 	interactive_play(cb);
 
