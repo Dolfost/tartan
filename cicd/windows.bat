e00be3a06b5e6e01ce7ce13bdb3867c97f1f2442
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-12-16 02:38:07 +0200

    Fix typo

--- cicd/windows.bat
+++ cicd/windows.bat
@@ -1,8 +1,9 @@
 IF [%1] == [] ECHO "No operation specified!" & EXIT 1
 
+SET "needdoxygen=0"
 IF [%1] EQU "docs" SET "needdoxygen=1"
 IF [%1] EQU "pack" SET "needdoxygen=1"
-IF %needdoxygen% EQU 1 SET "wingetdeps= doxygen graphviz texlive perl"
+IF %needdoxygen EQU 1 SET "wingetdeps= doxygen graphviz texlive perl"
 powershell -noexit "& ""C:\my_path\yada_yada\run_import_script.ps1"""
 
 :: installing dependencies
