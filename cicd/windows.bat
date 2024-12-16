8c3266946ae1341ea5f99f832932886cfe579a8b
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-12-16 02:40:10 +0200

    Remove some commands

--- cicd/windows.bat
+++ cicd/windows.bat
@@ -3,14 +3,13 @@ IF [%1] == [] ECHO "No operation specified!" & EXIT 1
 SET "needdoxygen=0"
 IF [%1] EQU "docs" SET "needdoxygen=1"
 IF [%1] EQU "pack" SET "needdoxygen=1"
-IF %needdoxygen EQU 1 SET "wingetdeps= doxygen graphviz texlive perl"
-powershell -noexit "& ""C:\my_path\yada_yada\run_import_script.ps1"""
+IF %needdoxygen% EQU 1 SET "wingetdeps= doxygen graphviz texlive perl"
 
 :: installing dependencies
 winget install cmake git %wingetdeps
 
 SET "SCRIPT_DIR=%~dp0"
 
-SET "REPO=%SCRIPT_DIR.."
+SET "REPO=%SCRIPT_DIR%.."
 
-cmake -P "%SCRIPT_DIR/cmake/$1.cmake"
+cmake -P "%SCRIPT_DIR%/cmake/$1.cmake"
