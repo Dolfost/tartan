a9cb309406fec929711f87a72261d60f97cedd97
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-12-16 14:06:10 +0200

    Add directo windows option

--- cicd/cmake/test.cmake
+++ cicd/cmake/test.cmake
@@ -3,7 +3,7 @@ if(WIN32)
 	set(CONFIG "-G 'MinGW Makefiles'")
 endif()
 execute_process( # configuration
-	COMMAND "${CMAKE_COMMAND}" ${CONFIG} -B build -S . -DTARTAN_TESTING=YES -DTARTAN_DOCS=NO
+	COMMAND "${CMAKE_COMMAND}" -G "MinGW Makefiles" -B build -S . -DTARTAN_TESTING=YES -DTARTAN_DOCS=NO
 	WORKING_DIRECTORY "$ENV{REPO}"
 )
 
