56a02549033e9b5399cf4d51a29fc89e9d3e05ac
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-12-16 13:46:46 +0200

    Use MinGW makefiles generetor when on WIN32

--- cicd/cmake/test.cmake
+++ cicd/cmake/test.cmake
@@ -1,6 +1,9 @@
 message("==>> Configuring...")
+if(WIN32)
+	set(CONFIG "-G \"MinGW Makefiles\"")
+endif()
 execute_process( # configuration
-	COMMAND "${CMAKE_COMMAND}" -B build -S . -DTARTAN_TESTING=YES -DTARTAN_DOCS=NO
+	COMMAND "${CMAKE_COMMAND}" ${CONFIG} -B build -S . -DTARTAN_TESTING=YES -DTARTAN_DOCS=NO
 	WORKING_DIRECTORY "$ENV{REPO}"
 )
 
