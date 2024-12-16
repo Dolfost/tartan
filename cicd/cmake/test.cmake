b601bffcdd6ec540ede2db20a79a485d933068f7
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-12-16 14:18:28 +0200

    Try to fix MinGW specification

--- cicd/cmake/test.cmake
+++ cicd/cmake/test.cmake
@@ -1,6 +1,6 @@
 message("==>> Configuring...")
 if(WIN32)
-	set(CONFIG "-G \\\"MinGW Makefiles\\\"")
+	set(CONFIG "-G \"MinGW Makefiles\"")
 endif()
 execute_process( # configuration
 	COMMAND "${CMAKE_COMMAND}" ${CONFIG} -B build -S . -DTARTAN_TESTING=YES -DTARTAN_DOCS=NO
